e3b0b9d6aef49272e98c1b241e9a08e938e969f8
Dan Dukeson <dandukeson@gmail.com>
2020-12-23 10:50:13 +0000

    string find is taking all the time

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,55 @@
 fn main() {
-    compress("e re redcar red car goo books good book");
+    compress(" the way. hello I was on my way to say hello to you but I missed you on the way. my way to say hello to you but I missed you on the way. hello I was on my way to say hello to you but I missed you on the way. my way to say hello to you but I missed you on the way. hello I was on my wayhello I was on my way to say hello to you but I missed you on the way. hello I was on my way to say hello to you but I missed you on the way.  my way to say hello to you but I missed you on the way. hello I was on my way to say hello to you but I missed you on the way. my way to say hello to you but I missed you on the way. hello I was on my way to say hello to you but I missed you on the way. my way to say hello to you but I missed you on the way. hello I was on my way to say hello to you but I missed you on the way. my way to say hello to you but I missed you on the way. hello I was on my way to say hello to you but I missed you on the way. my way to say hello to you but I missed you on the way. hello I was on my way to say hello to you but I missed you on the way. the more reptetitive a thing is the better job we can do of compressing it the way. hello I was on my way to say hello to you but I missed you on the way. my way to say hello to you but I missed you on the way. hello I was on my way to say hello to you but I missed you on the way. my way to say hello to you but I missed you on the way. hello I was on my wayhello I was on my way to say hello to you but I missed you on the way. hello I was on my way to say hello to you but I missed you on the way.  my way to say hello to you but I missed you on the way. hello I was on my way to say hello to you but I missed you on the way. my way to say hello to you but I missed you on the way. hello I was on my way to say hello to you but I missed you on the way. my way to say hello to you but I missed you on the way. hello I was on my way to say hello to you but I missed you on the way. my way to say hello to you but I missed you on the way. hello I was on my way to say hello to you but I missed you on the way. my way to say hello to you but I missed you on the way. hello I was on my way to say hello to you but I missed you on the way. the more reptetitive a thing is the better job we can do of compressing it");
+
+    // debug 2.28s
+    // release 1.02s
 }
 
 fn compress(input: &str) {
-    let max_window_len: usize = 8;
+    let max_window_len: usize = 1024;
+    let mut pos: usize = 0;
 
-    for pos in 0..input.len() {
+    while pos < input.len() {
         let lookahead_buf = &input[pos..];
-        let window_start = std::cmp::max(0, pos - max_window_len);
+        let window_start = pos.checked_sub(max_window_len).unwrap_or(0);
         let window = &input[window_start..pos];
+
+        let out = match find_match(&window, &lookahead_buf) {
+            Some(code) => {
+                pos += code.len;
+                Encoding::Code(code)
+            }
+            None => {
+                let c = lookahead_buf.chars().nth(0).unwrap();
+                pos += 1;
+                Encoding::Literal(c)
+            }
+        };
+        println!("{:?}", out);
     }
+}
+
+#[derive(Debug)]
+enum Encoding {
+    Literal(char),
+    Code(Code),
 }
 
 #[derive(Debug, Eq, PartialEq)]
 struct Code {
     dist: usize,
     len: usize,
-    // next: Option<char>,
 }
 
-fn find_match(search: &str, lookahead: &str) -> Code {
-    let mut code = Code {
-        dist: 0,
-        len: 0,
-        //        next: None,
-    };
-    println!("search is {}", search);
-    println!("lookahead is {}", lookahead);
+fn find_match(search: &str, lookahead: &str) -> Option<Code> {
+    let mut code: Option<Code> = None;
     for needle_len in 1..=lookahead.len() {
         let needle = &lookahead[0..needle_len];
-
-        println!("searching for {:?} in {:?}", needle, search);
         if let Some(found_at) = search.find(needle) {
-            code.len = needle_len;
-            code.dist = search.len() - found_at;
-            println!("found at {:?}", code);
-        } else {
-            println!("not found, anywhere");
+            code = Some(Code {
+                len: needle_len,
+                dist: search.len() - found_at,
+            });
         }
     }
     code
